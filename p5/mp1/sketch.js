fa4052fc288fadb0eb30e8a6d63efc79abb73454
Austin M <abmaybe@ilstu.edu>
2019-09-05 10:50:54 -0500

    mp1

--- p5/mp1/sketch.js
+++ p5/mp1/sketch.js
@@ -1,23 +1,84 @@
 function setup() {
-  // put setup code here
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(2000, 1350);
+  background(210)
 }
 
-function draw() {
+function mousePressed() {
 
-  background(237, 34, 93);
-    fill(0);
+  noStroke();
+  fill('#1261ff');
+  triangle(1164, 730, 1190, 730, 1190, 720);
 
-    if (mouseIsPressed) {
-      ellipse(width/2, height/2, 50, 50);
-    } else {
-      rect(width/2, height/2, 50, 50);
-    }
+  fill('#1261ff');
+  triangle(1164, 710, 1190, 710, 1190, 720);
 
-    print(mouseIsPressed);
+  stroke('#1261ff');
+  fill('#42fff2');
+  ellipse(1190, 720, 23, 23);
+
+  fill('#42fff2');
+  rect(1020, 750, 30, 30);
+
+  fill('#42fff2');
+  rect(1065, 750, 30, 30);
+
+  fill('#42fff2');
+  rect(1005, 665, 100, 100);
+
+  fill(255);
+  ellipse(1040, 697, 13, 13);
+
+  fill(255);
+  ellipse(1085, 697, 13, 13);
+
+  fill(0);
+  rect(1020, 690, 30, 7);
+
+  fill(0);
+  rect(1065, 690, 30, 7);
+
+  fill(0);
+  rect(1040, 725, 30, 5);
+
 }
 
 function mouseReleased() {
+
+  noStroke();
+  fill('#ffc517');
+  triangle(1164, 730, 1190, 730, 1190, 720);
+
+  fill('#ffc517');
+  triangle(1164, 710, 1190, 710, 1190, 720);
+
+  stroke('#ffc517');
+  fill('#ff6817');
+  ellipse(1190, 720, 23, 23);
+
+  stroke(0);
+  fill('#ff6817');
+  rect(1020, 750, 30, 30);
+
+  fill('#ff6817');
+  rect(1065, 750, 30, 30);
+
+  fill('#ff6817');
+  rect(1005, 665, 100, 100);
+
+  fill(255);
+  ellipse(1040, 697, 13, 13);
+
+  fill(255);
+  ellipse(1085, 697, 13, 13);
+
+  fill(0);
+  rect(1020, 690, 30, 7);
+
+  fill(0);
+  rect(1065, 690, 30, 7);
+
+  fill(0);
+  rect(1040, 725, 30, 5);
 
   console.log(mouseX + ',' + mouseY) ;
 }
