9d3e4dd7c533dbb6925a4f99b55815c17b82e67a
Austin M <abmaybe@ilstu.edu>
2019-10-11 19:57:49 -0500

    yoot

---
+++ p5/oop2-1/sketch.js
@@ -0,0 +1,52 @@
+var cars = [];
+
+function setup() {
+  // put setup code here
+  createCanvas(800, 800);
+  for (var i = 0; i < 100; i++) {
+  cars.push(new Car());
+}
+}
+
+function draw() {
+  // put drawing code here
+  background(0);
+
+  for (var i = 0; i < cars.length; i++) {
+  cars[i].display();
+  cars[i].drive();
+}
+fill('white');
+triangle(mouseX - 15, mouseY, mouseX, mouseY - 25, mouseX + 15, mouseY);
+
+}
+
+function Car() {
+  //attributes
+  this.pos = createVector(100, 100);
+  this.v = createVector(random(-5, 5), random(-5, 5));
+  this.r = random(255);
+  this.g = random(255);
+  this.b = random(255);
+  this.o = random(0);
+  this.wl = random(100);
+
+  //methods
+  this.display = function() {
+    stroke(this.r, this.g, this.b);
+    fill(0);
+    ellipse(this.pos.x, this.pos.y, this.wl, this.wl);
+  }
+
+  this.drive = function() {
+    this.pos.add(this.v);
+    if (this.pos.x > width) this.pos.x = 0;
+    if (this.pos.x < 0) this.pos.x = width;
+    if (this.pos.y > height) this.pos.y = 0;
+    if (this.pos.y < 0) this.pos.x = height;
+
+  }
+
+
+
+}
---
+++ p5/oop2/sketch.js
@@ -0,0 +1,67 @@
+var cars = [];
+var frogPos;
+
+function setup() {
+  // put setup code here
+  createCanvas(800, 800);
+  rectMode(CENTER) ;
+  for (var i = 0; i < 100; i++) {
+  cars.push(new Car());
+  }
+
+  frogPos = createVector(width/2, height-80);
+}
+
+function draw() {
+  // put drawing code here
+  background(100);
+
+  for (var i = 0; i < cars.length; i++) {
+  cars[i].display();
+  cars[i].drive();
+
+  //collision detection
+  if (cars[i].pos.dist(frogPos) < 20){
+    cars.splice(i , 1);
+    }
+  }
+
+//frog
+fill('green');
+ellipse(frogPos.x, frogPos.y, 50, 50);
+check4Keys();
+
+}
+
+function Car() {
+  //attributes
+  this.pos = createVector(100, 100);
+  this.v = createVector(random(-5, 5), random(-5, 5));
+  this.r = random(255);
+  this.g = random(255);
+  this.b = random(255);
+
+  //methods
+  this.display = function() {
+    fill(this.r, this.g, this.b);
+    rect(this.pos.x, this.pos.y, 100, 50);
+  }
+
+  this.drive = function() {
+    this.pos.add(this.v);
+    if (this.pos.x > width) this.pos.x = 0;
+    if (this.pos.x < 0) this.pos.x = width;
+    if (this.pos.y > height) this.pos.y = 0;
+    if (this.pos.y < 0) this.pos.x = height;
+
+  }
+
+}
+
+function check4Keys(){
+  if (keyIsDown(LEFT_ARROW)) frogPos.x = frogPos.x - 5;
+  if (keyIsDown(RIGHT_ARROW)) frogPos.x = frogPos.x + 5;
+  if (keyIsDown(UP_ARROW)) frogPos.y = frogPos.y - 5;
+  if (keyIsDown(DOWN_ARROW)) frogPos.y = frogPos.y + 5;
+
+}
