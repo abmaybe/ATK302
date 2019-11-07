1c33920256da4eb8b4928513a874cb2514bc9270
Austin M <abmaybe@ilstu.edu>
2019-11-07 11:41:08 -0600

    12

--- p5/googledata2/sketch.js
+++ p5/googledata2/sketch.js
@@ -3,7 +3,7 @@ var namesArray = [];
 function setup() {
 
   // Tabletop stuff, for getting google spreadsheet data in.
-  let url = '1GtE3eoYVWBv9zMPoyettXzDCEv6qdNGKio_hgEh5duM'; // this is KEY of the URL from the sheet
+  let url = '1dMs8-iyItR1HWjGY6pYovUrV7aFVxTJfldopE4b8ZIk'; // this is KEY of the URL from the sheet
   let settings = {
     key: url, // The url of the published google sheet
     callback: gotData, // A callback for when the data comes in
@@ -30,8 +30,8 @@ function gotData(data) {
 
   // iterate through the array of data and create an object and push it on an array called namesArray
   for (let i = 0; i < data.length; i++) {
-    namesArray.push(new Circle(data[i].Name, data[i].Shape)) ;
-   }
+    namesArray.push(new Purple(data[i].Name, data[i].Color));
+  }
 
 }
 
@@ -40,37 +40,49 @@ function draw() {
   background('blue');
 
   // // iterate through the namesArray and display the objects!
-  for (let i = 0 ; i < namesArray.length ; i++) {
-    namesArray[i].display() ;
+  for (let i = 0; i < namesArray.length; i++) {
+    namesArray[i].display();
     namesArray[i].drive();
   }
 
 }
 
+
 // my circle class
-function Circle(myName, myShape) {
+function Purple(myName, myColor) {
   this.pos = createVector(random(width), random(height));
-    this.vel = createVector(random(-5, 5), random(-5, 5));
+  this.vel = createVector(random(-5, 5), random(-5, 5));
   this.name = myName;
-  this.shape = myShape;
+  this.color = myColor;
 
 
   this.display = function() {
-    if (this.shape === "Circle") {
-    ellipse(this.pos.x, this.pos.y, 100, 100);
-  } else {
-    rect(this.pos.x, this.pos.y, 100, 100);
-  }
-    text(this.name, this.pos.x, this.pos.y);
-  }
-  this.drive = function() {
-    this.pos.add(this.vel);
+    if (this.color === "Purple") {
+      fill('purple');
+      ellipse(this.pos.x, this.pos.y, 100, 100);
+    }
+    if (this.color === "Red") {
+      fill('red');
+      ellipse(this.pos.x, this.pos.y, 100, 100);
+    }
+    if (this.color === "Green") {
+      fill('green');
+      ellipse(this.pos.x, this.pos.y, 100, 100);
+    }
+    //else {
+    //fill('yellow');
+    //rect(this.pos.x, this.pos.y, 100, 100);
+  
+  fill('black');
+  text(this.name, this.pos.x, this.pos.y);
+}
+this.drive = function() {
+  this.pos.add(this.vel);
 
-    if (this.pos.x > width) this.pos.x = 0;
-    if (this.pos.x < 0) this.pos.x = width;
-    if (this.pos.y > height) this.pos.y = 0;
-    if (this.pos.y < 0) this.pos.y = height;
+  if (this.pos.x > width) this.pos.x = 0;
+  if (this.pos.x < 0) this.pos.x = width;
+  if (this.pos.y > height) this.pos.y = 0;
+  if (this.pos.y < 0) this.pos.y = height;
 
-  }
-
+}
 }
